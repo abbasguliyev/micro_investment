439ba324ea02c95408fc5d9ae4dac08ac408e99d
Abbas Guliyev <abbasquliyev111@gmail.com>
2023-11-19 18:42:12 +0400

    new features

--- src/account/tasks.py
+++ src/account/tasks.py
@@ -1,11 +1,11 @@
 from celery import shared_task
-from account.api.selectors import user_balance_list, user_list
+from account.api.selectors import user_balance_list, user_list, investor_list
 from account.api.services import user_balance_create
 
 @shared_task(name="user_balance_create_task")
 def user_balance_create_task(user_id):
     user = user_list().filter(pk=user_id).last()
-    user_balance = user_balance_list().filter(user=user).exists()
-    print(f"{user_balance=}")
+    investor = investor_list().filter(user=user).last()
+    user_balance = user_balance_list().filter(user=investor).exists()
     if user_balance == False:
         user_balance_create(user=user)
