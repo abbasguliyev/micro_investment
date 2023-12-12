4f06c6af3fa92230557dc79191c5562e43d15998
Abbas Guliyev <abbasquliyev111@gmail.com>
2023-12-12 18:26:50 +0400

    cors

--- src/micro_investment/urls.py
+++ src/micro_investment/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
 from django.conf.urls.static import static
@@ -40,7 +40,7 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('api/v1/', include('micro_investment.api_routes'))
+    path('api/v1/', include('micro_investment.api_routes')),
     re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
