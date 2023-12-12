962bb523f009d1b42df271153f237faab6fb8057
Abbas Guliyev <abbasquliyev111@gmail.com>
2023-12-12 19:09:36 +0400

    cors

--- src/micro_investment/settings/base.py
+++ src/micro_investment/settings/base.py
@@ -55,8 +55,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -157,12 +157,9 @@ SWAGGER_SETTINGS = {
 }
 
 # Cors configuration
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_HEADERS = "*"
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS=True
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOWED_ORIGINS = [
-    'http://188.132.202.212:5173',
-    'https://halaekosistem.netlify.app'
-]
