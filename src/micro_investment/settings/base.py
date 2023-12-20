34f46ad172d30492e9356ab98ddf7b51bb336429
Abbas Guliyev <abbasquliyev111@gmail.com>
2023-12-20 17:35:04 +0400

    added password reset and chage futures

--- src/micro_investment/settings/base.py
+++ src/micro_investment/settings/base.py
@@ -222,8 +222,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'abbasquliyev111@gmail.com'
-EMAIL_HOST_PASSWORD = 'ejyd cgtc hreg zdif'
+EMAIL_HOST_USER = 'yaminsavalanli@gmail.com'
+EMAIL_HOST_PASSWORD = 'ckmw vvrm yjdu mtdm'
 
 DJANGO_REST_PASSWORDRESET = {
     'RESET_PASSWORD_TOKEN_TIMEOUT_DAYS': 1,
