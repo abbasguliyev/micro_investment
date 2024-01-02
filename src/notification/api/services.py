d81f3b06efd9983a80f5a38f6f2ded6935f2cfec
abbasguliyev <abbasquliyev111@gmail.com>
2024-01-02 08:27:18 +0400

    new features

--- src/notification/api/services.py
+++ src/notification/api/services.py
@@ -1,14 +1,28 @@
+from django.db import transaction
 from notification.models import Notification
-
+from notification.api.selectors import notification_list
+from notification.tasks import notification_all_read_task
 def notification_create(
         *, user,
-        message: str
+        message: str,
+        is_read: bool = False
 ) -> Notification:
     notification = Notification.objects.create(
         user=user,
-        message=message
+        message=message,
+        is_read=False
     )
     notification.full_clean()
     notification.save()
 
     return notification
+
+
+def notification_update(instance, **data) -> Notification:
+    notification = notification_list().filter(pk=instance.id).update(**data)
+    return notification
+
+def notification_all_read(user) -> bool:
+    print(f"{user=}")
+    transaction.on_commit(lambda: notification_all_read_task.delay(user.id))
+    return True
--- src/notification/models.py
+++ src/notification/models.py
@@ -4,6 +4,7 @@ class Notification(models.Model):
     user = models.ForeignKey("account.User", on_delete=models.CASCADE, related_name="notifications")
     message = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('-pk',)
